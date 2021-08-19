3a20d358a95c21bd0628dd1d1bde8a2522e0002c
othongonzaga <othoncesar1@hotmail.com>
2021-08-18 22:31:27 -0300

    mudando

--- otto-filmes/src/components/Header/index.js
+++ otto-filmes/src/components/Header/index.js
@@ -1,7 +1,7 @@
-import './Header.css'
+import './header.css'
 import {Link} from 'react-router-dom'
 
-export default function Header (){
+export default function header (){
     return(
         <header>
             <Link className='logo' to='/'>Filmes</Link>
