23f6ba867c06874af87c2e6314d000f5b66fc76d
othongonzaga <othoncesar1@hotmail.com>
2021-08-22 17:11:08 -0300

    Criando o Not Found

---
+++ otto-filmes/src/pages/Erro/index.js
@@ -0,0 +1,9 @@
+import './erro.css'
+
+export default function Erro(){
+    return(
+        <div>
+            <h1>Pagina Erro</h1>
+        </div>
+    )
+}
--- otto-filmes/src/routes.js
+++ otto-filmes/src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Header from './components/Header'
 import Filme from './pages/Filme'
 import Favoritos from './pages/Favoritos'
+import Erro from './pages/Erro'
 
 const Routes = () => {
   return(
@@ -11,7 +12,8 @@ const Routes = () => {
        <Switch>
          <Route exact path='/' component={Home}/> 
          <Route exact path='/filme/:id' component={Filme}/> 
-         <Route exact path='/favoritos' component={Favoritos}/> 
+         <Route exact path='/favoritos' component={Favoritos}/>
+         <Route path='*' component={Erro}/> 
        </Switch> 
     </BrowserRouter>
   )
