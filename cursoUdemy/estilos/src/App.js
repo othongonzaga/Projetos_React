bd44bcd686400c1975bd6212d024a7c20a28a85b
othongonzaga <othoncesar1@hotmail.com>
2021-08-24 12:06:53 -0300

    mudando

--- cursoUdemy/estilos/src/App.js
+++ cursoUdemy/estilos/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import {BemVindo, Container, Header, Título} from './styles'
 
 function App() {
   return (
-    <div className="container">
-      
-    </div>
+    <Container>
+      <Header>
+        <Título>Projeto</Título>
+      </Header>
+
+      <BemVindo cor='00FF00'>Bem Vindo ao sistema</BemVindo>
+    </Container>
   );
 }
 
 export default App;
+
+{/* <div className="container">
+<header className='header'>
+  <a className='título'>Projeto Styled</a>
+</header>
+
+<h1>Bem vindo!</h1>
+</div> */}
