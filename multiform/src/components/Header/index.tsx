aaf6e7fd3216aa3ce1c64a3cb6c25c88ccc53e23
othongonzaga <othoncesar1@hotmail.com>
2022-01-17 19:13:06 -0300

    Parte 2 do código(criando components, fazendo as pages, a Theme e outras mudanças)

---
+++ multiform/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const Header = () => {
+    return (
+        <C.Container>
+            <h1>Cadastro de Programador</h1>
+            <p>Faça seu cadastro na lista e receba propostas de emprego.</p>
+        </C.Container>
+    );
+}
--- multiform/src/pages/FormStep2/index.tsx
+++ multiform/src/pages/FormStep2/index.tsx
@@ -1,9 +1,68 @@
-import * as C from './styles'
+import { useNavigate, Link } from 'react-router-dom';
+import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
+import { Theme } from '../../components/Theme';
+import { ChangeEvent, useEffect } from 'react';
+import { SelectOption } from '../../components/SelectOption';
 
-export const FormStep2 = () =>{
-    return(
-        <C.Container>
-            Passo 2
-        </C.Container>
-    )
+export const FormStep2 = () => {
+    const navegate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        if(state.name === '') {
+            navegate('/');
+        } else {
+            dispatch({
+                type: FormActions.setCurrentStep,
+                payload: 2
+            });
+        }
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== '') {
+            navegate('/step3');
+        } else {
+            alert("Preencha os dados.");
+        }
+    }
+
+    const setLevel = (level: number) => {
+        dispatch({
+            type: FormActions.setLevel,
+            payload: level
+        });
+    }
+
+    return (
+        <Theme>
+            <C.Container>
+                <p>Passo 2/3</p>
+                <h1>{state.name}, o que melhor descreve você?</h1>
+                <p>Escolha a opção que melhor condiz com seu estado atual, profissionalmente.</p>
+
+                <hr/>
+
+                <SelectOption
+                    title="Sou iniciante"
+                    description="Comecei a programar há menos de 2 anos"
+                    icon="🥳"
+                    selected={state.level === 0}
+                    onClick={()=>setLevel(0)}
+                />
+
+                <SelectOption
+                    title="Sou programador"
+                    description="Já programo há 2 anos ou mais"
+                    icon="😎"
+                    selected={state.level === 1}
+                    onClick={()=>setLevel(1)}
+                />
+
+                <Link to="/" className="backButton">Voltar</Link>
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
+    );
 }
