9bdda72b15a9c644fa404811a12ae0f57d43468d
othongonzaga <othoncesar1@hotmail.com>
2022-02-15 15:45:24 -0300

    Projeto Sistemas de Finanças

---
+++ sistema-financas/src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+    {date: new Date(2022, 1, 13), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2022, 1, 13), category: 'food', title: 'Burger', value: 28},
+    {date: new Date(2022, 1, 13), category: 'rent', title: 'Aluguel', value: 2300},
+    {date: new Date(2022, 2, 13), category: 'salary', title: 'Salário', value: 4500}
+]
