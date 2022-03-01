1f5bcb70d1bc9ceeec8cad7c9ece456cc3474822
othongonzaga <othoncesar1@hotmail.com>
2022-03-01 17:59:48 -0300

    Mudanças do react router v6

--- cgp/src/App.js
+++ cgp/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Home} from './components/pages/Home'
 import {Company} from './components/pages/Company'
 import {Contact} from './components/pages/Contact'
@@ -13,28 +13,16 @@ function App() {
   return (
    <Router>
      <Navbar/>
-     <Switch>
        <Container customClass="min-height">
-         <Route exact path="/">
-           <Home/>
-         </Route>
-         <Route path="/projects">
-           <Projects/>
-         </Route>
-         <Route path="/company">
-           <Company/>
-         </Route>
-         <Route path="/contact">
-           <Contact/>
-         </Route>
-         <Route  path="/newproject">
-           <NewProject/>
-         </Route>
-         <Route path="/project/:id">
-           <Project/>
-         </Route>
+     <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/projects" element={<Projects/>}/>
+         <Route path="/company" element={<Company/>}/>
+         <Route path="/contact" element={<Contact/>}/>
+         <Route  path="/newproject" element={<NewProject/>}/>
+         <Route path="/project/:id" element={<Project/>}/>
+     </Routes>
        </Container>
-     </Switch>
      <Footer/>
    </Router>
   );
